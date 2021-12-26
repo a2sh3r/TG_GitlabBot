6748b79fd68acb69e5ae3ce1eadd3fb50f13bf75
a2sh3r <ashurmetovmm@gmail.com>
2021-12-26 18:18:12 +0300

    added issue sending method

--- botGit.py
+++ botGit.py
@@ -1,32 +1,21 @@
 import telebot #Библиотека с тг ботом
 import gitlab
-import csv
 
 
 #Работа с гит лаб
 
 
+user_name = ''
 gitlabUrl= 'https://gitlab.com/'
 token = 'glpat-cDC1Za7UVWJFmTcYhytJ'
-projectId = 32385489
-
+projectId = 32387910
+issue_title = []
+closed_issue_title = []
 
 gl = gitlab.Gitlab(gitlabUrl, token)
 gl.auth()
 
-project = gl.projects.get(projectId)
-issues = project.issues.list()
-for issue in issues:
-        print (issue.iid)
-
-
 botGit = telebot.TeleBot('5051671475:AAEPs3doUsurVa69iql8N_I1aBoPo5myNBs')  #токен
-
-
-repName = ''
-user_name = ''
-
-
 
 @botGit.message_handler(content_types=['text']) #декоратор для метода получения сообщений ботом
 
@@ -34,14 +23,19 @@ def start(message):
     if message.text == "/start":
         botGit.send_message(message.from_user.id, "Как тебя зовут?")
         botGit.register_next_step_handler(message, get_name)
-    elif message.text=="/url":
-        botGit.send_message(message.from_user.id, repName)
+    elif message.text=="/id":
+        botGit.send_message(message.from_user.id, projectId)
+        #botGit.send_message(message, list_issue)
     elif message.text=="/name":
         botGit.send_message(message.from_user.id, user_name)
+    elif message.text=="/score":
+        for issue in issue_title:
+            botGit.send_message(message.from_user.id, issue)
     elif message.text=="/help":
-        botGit.send_message(message.from_user.id, "/start - начало работы с ботом \n/url - показать ссылку на репозиторий \n/name - показать имя")
+        botGit.send_message(message.from_user.id, "/start - начало работы с ботом \n/score - показать проблемы \n/name - показать имя")
     else: 
         botGit.send_message(message.from_user.id,"Напиши /help")
+
     
 
 
@@ -52,8 +46,23 @@ def get_name (message):
     botGit.register_next_step_handler(message, get_url)
 
 def get_url(message):
-    global repName
-    repName = message.text
+    global projectId
+    projectId = message.text
+
+def list_issue():
+    global issue_title
+    global closed_issue_title
+    project = gl.projects.get(projectId)
+    issues = project.issues.list()
+    closed_issues = gl.issues.list(state='closed')
+    for issue in issues:
+        issue_title.append(issue.title)
+    for issue in closed_issues:
+        closed_issue_title.append(issue.title)
+    print(issue_title)
+    print(closed_issue_title)
+
+list_issue()
 
 botGit.polling(none_stop=True, interval=0) #бот чекает пришло ли ему сообщение постоянно
 
