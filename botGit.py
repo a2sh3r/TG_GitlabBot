fe089c1141bb498ff5d5181dca1b739f655e396c
a2sh3r <ashurmetovmm@gmail.com>
2021-12-26 13:58:30 +0300

    reg moment

--- botGit.py
+++ botGit.py
@@ -1,18 +1,42 @@
 import telebot #Библиотека с тг ботом
 botGit = telebot.TeleBot('5051671475:AAEPs3doUsurVa69iql8N_I1aBoPo5myNBs')  #токен
 
+repName = ''
+user_name = ''
+
+
 @botGit.message_handler(content_types=['text']) #декоратор для метода получения сообщений ботом
-def get_text_messages(message):
+
+def start(message):
     if message.text == "/start":
-        botGit.send_message(message.from_user.id, "Салам, для начала работы поздоровайтесь со мной, но только козырно)")
-    elif message.text == "Салам":
-        botGit.send_message(message.from_user.id, "Чем могу быть полезен, сударь?")
-    elif message.text == "/help":
-        botGit.send_message(message.from_user.id, "Напиши Салам")
-    else:
-        botGit.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+        botGit.send_message(message.from_user.id, "Как тебя зовут?")
+        botGit.register_next_step_handler(message, get_name)
+    elif message.text=="/url":
+        botGit.send_message(message.from_user.id, repName)
+    elif message.text=="/name":
+        botGit.send_message(message.from_user.id, user_name)
+    elif message.text=="/help":
+        botGit.send_message(message.from_user.id, "/start - начало работы с ботом \n/url - показать ссылку на репозиторий \n/name - показать имя")
+    else: 
+        botGit.send_message(message.from_user.id,"Напиши /help")
     
+def get_name (message):
+    global user_name
+    user_name = message.text
+    botGit.send_message(message.from_user.id, 'Дай ссылку на свой репозиторий')
+    botGit.register_next_step_handler(message, get_url)
+
+def get_url(message):
+    global repName
+    repName = message.text
+
+
+
 botGit.polling(none_stop=True, interval=0) #бот чекает пришло ли ему сообщение постоянно
+
+
+
+
 
 
 
