2ab0263ae03916b338a53e487ca2cc1d620b8c11
a2sh3r <ashurmetovmm@gmail.com>
2021-12-26 09:32:07 +0300

    Ого, оно работает с первого раза

--- botGit.py
+++ botGit.py
@@ -1,3 +1,18 @@
-import telebot
-botGit = telebot.TeleBot('5051671475:AAEPs3doUsurVa69iql8N_I1aBoPo5myNBs')
+import telebot #Библиотека с тг ботом
+botGit = telebot.TeleBot('5051671475:AAEPs3doUsurVa69iql8N_I1aBoPo5myNBs')  #токен
+
+@botGit.message_handler(content_types=['text']) #декоратор для метода получения сообщений ботом
+def get_text_messages(message):
+    if message.text == "/start":
+        botGit.send_message(message.from_user.id, "Салам, для начала работы поздоровайтесь со мной, но только козырно)")
+    elif message.text == "Салам":
+        botGit.send_message(message.from_user.id, "Чем могу быть полезен, сударь?")
+    elif message.text == "/help":
+        botGit.send_message(message.from_user.id, "Напиши Салам")
+    else:
+        botGit.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+    
+botGit.polling(none_stop=True, interval=0) #бот чекает пришло ли ему сообщение постоянно
+
+
 
