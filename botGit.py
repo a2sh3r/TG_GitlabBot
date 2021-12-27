b3185a595f822daa105335b3236b05ee4b9de93f
a2sh3r <ashurmetovmm@gmail.com>
2021-12-27 18:05:37 +0300

    Исправление ошибок + интеграция базы данных SQLite

--- botGit.py
+++ botGit.py
@@ -1,55 +1,77 @@
-import telebot #Библиотека с тг ботом
+import telebot #Библиотека с тг ботом, гитлабом и бдшками
 import gitlab
+import sqlite3
 
-
-#Работа с гит лаб
-
-
-user_name = ''
+user_name = ''  #все переменные 
 gitlabUrl= 'https://gitlab.com/'
 token = 'glpat-cDC1Za7UVWJFmTcYhytJ'
 projectId = 32387910
 issue_title = []
 closed_issue_title = []
+gluser_list=[]
 
-gl = gitlab.Gitlab(gitlabUrl, token)
+def db_execute():  #создание бд, если ее нет и добавление в нее пользователей, пока не реализовано до конца
+    global gluser_list
+    global user_name
+    global projectId
+    connect = sqlite3.connect('glusers.db')
+    cursor = connect.cursor()
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS glusers
+    (gluser_name TEXT, gluser_id INTEGER)''')
+
+    connect.commit()
+
+    gluser_list.append(user_name)
+    gluser_list.append(projectId)
+    cursor.execute("INSERT INTO glusers VALUES(?,?);",gluser_list)
+    connect.commit()
+    gluser_list.clear()
+
+gl = gitlab.Gitlab(gitlabUrl, token)  #аутентификация гитлаба, для дальнейшей работы с проектами и issue
 gl.auth()
 
 botGit = telebot.TeleBot('5051671475:AAEPs3doUsurVa69iql8N_I1aBoPo5myNBs')  #токен
 
 @botGit.message_handler(content_types=['text']) #декоратор для метода получения сообщений ботом
 
-def start(message):
+def start(message): #обработка сообщений ботом
     if message.text == "/start":
         botGit.send_message(message.from_user.id, "Как тебя зовут?")
         botGit.register_next_step_handler(message, get_name)
     elif message.text=="/id":
-        botGit.send_message(message.from_user.id, projectId)
-        #botGit.send_message(message, list_issue)
+        botGit.register_next_step_handler(message, get_id)
     elif message.text=="/name":
         botGit.send_message(message.from_user.id, user_name)
     elif message.text=="/score":
         for issue in issue_title:
             botGit.send_message(message.from_user.id, issue)
     elif message.text=="/help":
-        botGit.send_message(message.from_user.id, "/start - начало работы с ботом \n/score - показать проблемы \n/name - показать имя")
+        botGit.send_message(message.from_user.id, "/start - начало работы с ботом \n/score - показать issue \n/id поменять айди проекта")
     else: 
         botGit.send_message(message.from_user.id,"Напиши /help")
 
-    
-
-
-def get_name (message):
+def get_name (message): #метод получения имени польщователя
     global user_name
     user_name = message.text
-    botGit.send_message(message.from_user.id, 'Дай ссылку на свой репозиторий')
+    botGit.send_message(message.from_user.id, 'Дай айди своего проекта')
     botGit.register_next_step_handler(message, get_url)
 
-def get_url(message):
+def get_url(message): #метод получения токена пользователя
     global projectId
     projectId = message.text
+    db_execute()
+    list_issue()
 
-def list_issue():
+def get_id(message): #метод получения токена пользователя
+    global projectId, issue_title, closed_issue_title
+    issue_title.clear()
+    closed_issue_title.clear()
+    projectId = message.text
+    db_execute()
+    list_issue()
+
+def list_issue(): #обработка и выдача информации по закрытым issue в проекте, указанном пользователем
     global issue_title
     global closed_issue_title
     project = gl.projects.get(projectId)
@@ -59,10 +81,6 @@ def list_issue():
         issue_title.append(issue.title)
     for issue in closed_issues:
         closed_issue_title.append(issue.title)
-    print(issue_title)
-    print(closed_issue_title)
-
-list_issue()
 
 botGit.polling(none_stop=True, interval=0) #бот чекает пришло ли ему сообщение постоянно
 
